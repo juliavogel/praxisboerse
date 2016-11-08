005cc984501e8c2e2e3b7393ef1f48eb3d1d8b68
Julia Vogel <julia.vogel.kehl@web.de>
2016-11-08 12:53:06 +0100

    Fixed bug in header dropdowns - data wouldn't load on first page load

--- app/modules/menu/menu-controllers.js
+++ app/modules/menu/menu-controllers.js
@@ -3,31 +3,7 @@
 angular.module('menu')
 
     .controller('HeaderController',
-        function ($scope, $location, OfferService) {
-
-            OfferService.listOfferTypes(function(response) {
-                if (response.success) {
-                    $scope.typeOptions = [];
-                    for (var key in response.data) {
-                        $scope.typeOptions.push({
-                            value: response.data[key].shortname,
-                            name: response.data[key].name
-                        });
-                    }
-                }
-            });
-
-            OfferService.listCountries(function(response) {
-                if (response.success) {
-                    $scope.countryOptions = [];
-                    for (var key in response.data) {
-                        $scope.countryOptions.push({
-                            value: response.data[key].code,
-                            name: response.data[key].name
-                        });
-                    }
-                }
-            });
+        function ($rootScope, $scope, $location, OfferService) {
 
             $scope.loggedIn = function() {
                 return ($location.path() !== '/login');
@@ -52,6 +28,37 @@ angular.module('menu')
             $scope.setOfferSearch = function(inputSearch) {
                 OfferService.offerFilter.offerSearch = inputSearch;
             };
+
+            $scope.initDropdowns = function() {
+                OfferService.listOfferTypes(function(response) {
+                    if (response.success) {
+                        $scope.typeOptions = [];
+                        for (var key in response.data) {
+                            $scope.typeOptions.push({
+                                value: response.data[key].shortname,
+                                name: response.data[key].name
+                            });
+                        }
+                    }
+                });
+                OfferService.listCountries(function(response) {
+                    if (response.success) {
+                        $scope.countryOptions = [];
+                        for (var key in response.data) {
+                            $scope.countryOptions.push({
+                                value: response.data[key].code,
+                                name: response.data[key].name
+                            });
+                        }
+                    }
+                });
+            };
+
+            $rootScope.$on('$routeChangeSuccess', function(event, current, previous) {
+                if ((typeof previous === 'undefined' || previous.originalPath === '/login') && (current.originalPath === '/offer' || current.originalPath === '/bookmark')) {
+                    $scope.initDropdowns();
+                }
+            });
 
         });
 
